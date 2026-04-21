85663715aefadc841f2e925e081b59ecafd70bc5
欣宜林 <aslo293102@gmail.com>
2026-04-22 00:39:41 +0800

    修改寄email驗證信

--- demo/demo/src/main/java/com/example/demo/service/EmailService.java
+++ demo/demo/src/main/java/com/example/demo/service/EmailService.java
@@ -1,4 +1,3 @@
-// 記得補上 package 和 import
 package com.example.demo.service;
 
 import org.springframework.mail.SimpleMailMessage;
@@ -12,13 +11,13 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
-    public void sendVerificationEmail(String to, String code) {
-    	String verifyUrl = "http://localhost:9990/api/auth/verify?code=" + code;
+    public void sendVerificationEmail(String toEmail, String token) {
+    	String verifyUrl = "http://localhost:8001/api/members/verify?token=" + token;
     	
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("djangodtl123@gmail.com");
-        message.setTo(to);
-        message.setSubject("TECO 購物通 - 會員帳號驗證");
+        message.setTo(toEmail);
+        message.setSubject("LifeStyle 購物 - 會員帳號驗證");
         message.setText("您好：\n\n感謝您的註冊！請點擊以下連結以啟用您的帳號：\n" + verifyUrl);
         mailSender.send(message);
     }
